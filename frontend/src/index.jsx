35cd680899e178c297857936763af819dd60ed56
mendi <mmfriedman16@gmail.com>
2022-05-29 22:28:29 +0300

    clean and add comments

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import * as ReactDOM from 'react-dom/client';
 import { App } from './app';
 
-
+// creates a Document Object Module to control the app
 const root = ReactDOM.createRoot(
     document.getElementById('root')
 )
-root.render(<App/>)
+
+// render app components
+root.render(
+    <App />
+)
