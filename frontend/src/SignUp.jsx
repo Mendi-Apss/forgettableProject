83b60cb3ea0cc7611ca9a7f691dbf8a5a5656f3f
mendi <mmfriedman16@gmail.com>
2022-06-13 17:55:22 +0300

    update signup component

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -4,34 +4,42 @@ const Axios = require('axios')
 
 export const SignUpForm = () => {
 
+    const [email, setEmail] = useState('')
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
+    
 
     const sendUserData = () => {
         const user = {
-            userName : userName,
-            password : password
+            email : email,
+            userName: userName,
+            password: password
         }
         console.log(user);
 
-        Axios.post('http://localhost:8080/',user)
+        Axios.post('http://localhost:8080/sign-up', user)
 
-        
+
     }
 
     return (
         <div className="sign-up">
             <form id="sign-up-form" onSubmit={sendUserData}>
-                <label for="sign-up-form"> Sign Up</label>
+                <label for="login-sign-up-form"> Sign Up</label>
 
-                <input type="email" id="sign-up-user-name" placeholder="User name"
-                 value={userName} onChange={element => setUserName(element.target.value)} /> <br />
+                <input type="email" id="sign-up-user-name" placeholder="Email"
+                    value={email} onChange={element => setEmail(element.target.value)} /> <br />
+
+                <input type="text" id="sign-up-username" placeholder="User name"
+                    value={userName} onChange={element => setUserName(element.target.value)} /><br />
 
                 <input type="password" id="sign-up-password" placeholder="Password"
-                 value={password} onChange={element => setPassword(element.target.value)} /><br />
+                    value={password} onChange={element => setPassword(element.target.value)} /><br />
 
                 <button type="submit" id="sign-up-button">Sign Up</button>
             </form>
         </div>
+
+
     )
 }
