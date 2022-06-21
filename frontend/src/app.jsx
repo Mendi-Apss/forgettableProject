86704aac65247c6a03e821fdac328390f68d58db
mendi <mmfriedman16@gmail.com>
2022-06-21 09:20:35 +0300

    add routing

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,15 +1,30 @@
 import { Login } from "./Login"
 import { SignUpForm } from "./SignUp"
 import './stylesheet/app.css'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { Home } from "./Home"
 
 
 export const App = () => {
 
     return (
-        <div id="app">
-            <input type="checkbox" id="login-sign-up-form" aria-hidden="true"></input>
-                <Login/>
-                <SignUpForm/>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path={'/'}>
+                    <Redirect to={'/login'} />
+                </Route>
+                <Route exact path={'/login'}>
+                    <div id="app">
+                        <input type="checkbox" id="login-sign-up-form" aria-hidden="true"></input>
+                        <Login />
+                        <SignUpForm />
+                    </div>
+                </Route>
+                <Route exact path='/home'>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+
     )
 }
