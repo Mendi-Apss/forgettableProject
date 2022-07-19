a5a73694850ffd6b7fed4e288e601fda7b851734
Yosefsilly <Yosefsilberg@gmail.com>
2022-07-19 18:57:38 +0300

    add login form and one state rather than two

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,62 +1,82 @@
-import './stylesheet/login.css'
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Chip } from '@mui/material'
-import { useCookies } from 'react-cookie';
-const axios = require('axios')
+import "./stylesheet/login.css";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Chip } from "@mui/material";
+import { useCookies } from "react-cookie";
+const axios = require("axios");
 
 export const Login = () => {
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  const [error, setError] = useState(null);
+  const [cookies, setCookie] = useCookies(null);
+  const page = useHistory();
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(null)
-    const [cookies, setCookie] = useCookies(null);
-    const page = useHistory()
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const errorMassege = () => {
-        return (
-            <Chip label='Wrong email or password' id='error-massege' />
-        )
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    const submitLogin = () => {
-        const user = {
-            email: email,
-            password: password
-        }
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
 
-        axios.post('http://localhost:8080/login', user)
-            .then((res) => {
-                if (res.data.ifThrIsPermissions) {
-                    console.log(res.data);
-                    setCookie('user', res.data.userForCookie,{path:'/'})
-                    page.push('/home')
-                }
-                else {
-                    setError(errorMassege)
-                    setEmail('')
-                    setPassword('')
-                }
-            })
-    }
+  const errorMassege = () => {
+    return <Chip label="Wrong email or password" id="error-massege" />;
+  };
 
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    return (
-        <div className="login">
-            <div id="login-form">
-                <label htmlFor='login-sign-up-form'>Login</label>
+    axios.post("http://localhost:8080/login", formData).then((res) => {
+      if (res.data.ifThrIsPermissions) {
+        console.log(res.data);
+        setCookie("user", res.data.userForCookie, { path: "/" });
+        page.push("/home");
+      } else {
+        setError(errorMassege);
+        setFormData({
+          email: "",
+          password: "",
+        });
+      }
+    });
+  }
 
-                <input type="email" id="login-user-name" placeholder="Email"
-                    value={email} onChange={element => setEmail(element.target.value)} /> <br />
-
-                <input type="password" id="login-password" placeholder={'Password'}
-                    value={password} onChange={element => setPassword(element.target.value)} /><br />
-
-                <div id='error-popup-massege'> {error} </div>
-
-                <button id="login-button" onClick={submitLogin}>Login</button>
-
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="login">
+        <div id="login-form">
+          <label htmlFor="login-sign-up-form">Login</label>
+          <input
+            type="email"
+            id="login-user-name"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />{" "}
+          <br />
+          <input
+            type="password"
+            id="login-password"
+            placeholder={"Password"}
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <br />
+          <div id="error-popup-massege"> {error} </div>
+          <button id="login-button">Login</button>
         </div>
-    )
-}
+      </div>
+    </form>
+  );
+};
