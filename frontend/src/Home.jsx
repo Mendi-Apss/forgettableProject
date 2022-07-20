0dfbe33bcbd404ecf7e7c87b15b505e64f6a4336
mendi <mmfriedman16@gmail.com>
2022-07-20 13:05:06 +0300

    add home component

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,23 +1,11 @@
-import { Avatar, Chip, AppBar, Container } from '@mui/material'
-import { useCookies } from "react-cookie";
-import './stylesheet/appBar.css'
+import {MenuBar} from './MenueBar'
 
 
 export const Home = () => {
 
-    const [cookie, setCookie] = useCookies()
-
-    const MenuBar = () => {
-
-    }
+ 
 
     return (
-        <AppBar id='app-bar'>
-            <Chip id='user-chip' avatar={
-                <Avatar id='user-avatar' sx={{ width: 70, height: 70 }}>
-                    {localStorage.getItem('user-name').charAt(0).toUpperCase()}
-                </Avatar>
-            } label={`Hello ${localStorage.getItem('user-name')}`} />
-        </AppBar>
+        <MenuBar/>
     )
 }
