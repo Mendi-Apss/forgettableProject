2066283d8d93fcc8e3ab2b0dffcb654b891bf5f0
mendi <mmfriedman16@gmail.com>
2022-07-21 20:02:54 +0300

    add secret and security

--- backend/authentication.js
+++ backend/authentication.js
@@ -1,5 +1,10 @@
 const jwt = require('jsonwebtoken')
+const secret = require('./dbConnection')
 
 module.exports.authentication = (data) => {
-    return jwt.sign(data.userName, 'mendi')
+    return jwt.sign(data.userName, secret.secret)
+}
+
+module.exports.checkToken = (userToken) => {
+    return jwt.verify(userToken,secret.secret)
 }
